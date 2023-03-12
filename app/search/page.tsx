4ecb3d4ad20adaf23a84fe2bb02beb23f0fe1539
Lucas Enriquez <lucasenriquez190502@gmail.com>
2023-03-12 19:33:05 -0300

    Fix search feature

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { useSearchParams } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import MasonryLayout from '../../components/MasonryLayout/MasonryLayout'
 import Search404 from '../../components/Search-404/Search-404'
@@ -6,16 +7,18 @@ import Spinner from '../../components/Spinner/Spinner'
 import { searchQuery } from '../../utils/data'
 import { client } from '../client'
 
-function search({params, searchParams}) {
+function search() {
   const [error, setError] = useState(null)
   const [results, setResults] = useState(null)
-  const searchTerm = searchParams?.q
 
-  const capitalizedTerm = searchTerm?.charAt(0).toUpperCase() + searchTerm?.slice(1)
+  const paramsOne = useSearchParams();
+  const search = paramsOne.get('q')
+
+  const capitalizedTerm = search?.charAt(0).toUpperCase() + search?.slice(1)
 
   const fetchResults = async () => {
     try {
-      const foundResults = await client.fetch(searchQuery(searchTerm))
+      const foundResults = await client.fetch(searchQuery(search))
       setResults(foundResults)
     } catch (error) {
       console.log(error)
@@ -25,12 +28,10 @@ function search({params, searchParams}) {
 
   useEffect(() => {
     fetchResults()
-  }, [searchTerm])
+  }, [search])
 
-  console.log(searchParams.q)
-  console.log(searchTerm)
 
-  if(!searchParams.q || !searchTerm) return <Search404/>
+  if(!search) return <Search404/>
   if(results === null) return <Spinner message={`We are loading your results for: ${capitalizedTerm}`}/>
   if(results.length === 0) return (<div className='text-center mt-10 text-2xl'>No pins found!</div>)
   return (
